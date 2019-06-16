d27f160b2bfadb74b1944957a6ddb5c4475ed611
unknown <meettomangesh@gmail.com>
2019-06-17 01:46:13 +0530

    To save work

--- folio/src/app/app-routing.module.ts
+++ folio/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BannerComponent } from './banner/banner.component';
+import { AttributeSliderComponent } from './attribute-slider/attribute-slider.component';
+import { PortfolioItemComponent } from './portfolio-item/portfolio-item.component';
+import { JournalItemComponent } from './journal-item/journal-item.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BannerComponent },
+  { path: 'about-us', component: AttributeSliderComponent },
+  { path: 'portfolio', component: PortfolioItemComponent },
+  { path: 'journal', component: JournalItemComponent },
+  { path: 'contact-us', component: ContactUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
